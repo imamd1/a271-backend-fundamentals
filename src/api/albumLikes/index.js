771338d6e4e1feb23dc1bd491d580d92b5256e59
Imam D <sakurakun002@gmail.com>
2024-10-24 19:41:38 +0700

    fix submission v3 criteria

--- src/api/albumLikes/index.js
+++ src/api/albumLikes/index.js
@@ -5,10 +5,10 @@ const routes = require('./routes');
 module.exports = {
   name: 'albumLikes',
   version: '1.0.0',
-  register: async (server, { service, albumsService, cacheService, validator }) => {
+  register: async (server, { albumLikesService, albumsService, cacheService, validator }) => {
  
     const albumLikesHandler = new AlbumLikesHandler(
-      service,
+      albumLikesService,
       albumsService, cacheService,
       validator,
     );
